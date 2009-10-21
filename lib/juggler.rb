1e2f78520e3b99034043b060cdf5546d73e6bd18
Ben Pickles <spideryoung@gmail.com>
2009-10-21 17:44:34 +0800

    Add configurable logger.

--- lib/juggler.rb
+++ lib/juggler.rb
@@ -1,7 +1,20 @@
 require 'beanstalk-client'
 
+autoload :Logger, 'logger'
+
 class Juggler
   class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= begin
+        logger = Logger.new(STDOUT)
+        logger.level = Logger::WARN
+        logger.debug("Created logger")
+        logger
+      end
+    end
+
     def throw(method, params, options = {})
       # TODO: Do some checking on the method
       connection.use(method.to_s)
